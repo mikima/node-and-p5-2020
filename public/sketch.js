531263e275ba74395b06e6356e3b224e1b361fc4
mikima <mikimauri@gmail.com>
2020-11-08 00:12:37 +0100

    added sketch

---
+++ public/sketch.js
@@ -0,0 +1,14 @@
+function preload() {
+  // put preload code here
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // put setup code here
+  background("black");
+}
+
+function draw() {
+  // put drawing code here
+  ellipse(mouseX, mouseY, 30);
+}
--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 console.log("up and running");
+
+let express = require("express");
+
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port);
+
+app.use(express.static("public"));

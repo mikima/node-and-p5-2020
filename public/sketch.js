6ef91ee3a0c61cb9865287c2b89b0cb6cfe6e34c
mikima <mikimauri@gmail.com>
2020-11-08 01:29:38 +0100

    added sockets

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,10 @@
+let color, size;
+
+let socket = io();
+
+socket.on("init", initFunction);
+socket.on("mouseBroadcast", otherMouse);
+
 function preload() {
   // put preload code here
 }
@@ -6,9 +13,29 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   // put setup code here
   background("black");
+  console.log("socket:", socket.id);
+}
+
+function initFunction(data) {
+  console.log("init", data);
+  color = data.color;
+  size = data.size;
+  initialised = true;
+}
+
+function otherMouse(data) {
+  console.log("received", data);
+  fill(data.color);
+  ellipse(data.x, data.y, data.size);
 }
 
 function draw() {
   // put drawing code here
-  ellipse(mouseX, mouseY, 30);
+}
+
+function mouseMoved() {
+  fill(color);
+  ellipse(mouseX, mouseY, size);
+  let message = { x: mouseX, y: mouseY, color: color, size: size };
+  socket.emit("mouse", message);
 }
--- server.js
+++ server.js
@@ -1,11 +1,61 @@
-console.log("up and running");
+// define the possible colors
 
+const CSS_COLOR_NAMES = [
+  "AliceBlue",
+  "AntiqueWhite",
+  "Aqua",
+  "Aquamarine",
+  "Azure",
+  "Beige",
+  "Bisque",
+  "BlanchedAlmond",
+  "Blue",
+  "BlueViolet",
+  "Brown",
+  "BurlyWood",
+  "CadetBlue",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+];
+
+// import express
 let express = require("express");
-
+// import socket.io
+let socket = require("socket.io");
+// initialise express
 let app = express();
-
+// choose port number
 let port = 3000;
-
+// start the server
 let server = app.listen(port);
-
+// define the folde that will be used
+// to serve files
 app.use(express.static("public"));
+// initialise socket.io
+let io = socket(server);
+// when there is a new connection,
+// call the "newConnection" function
+io.on("connect", onConnect);
+
+// create the "newConnection" function
+function onConnect(socket) {
+  // print the id of the new connection
+  console.log("new connection:", socket.client.id);
+  // select a color
+  let color =
+    CSS_COLOR_NAMES[Math.floor(Math.random() * CSS_COLOR_NAMES.length)];
+  // choose a size
+  let size = Math.random() * 40 + 5;
+  //send them the client
+  socket.emit("init", { color: color, size: size });
+  // when the client send a message called "mouse"
+  // exectute the function "mouseMessage"
+  socket.on("mouse", mouseMessage);
+
+  // create the mouseMessage
+  function mouseMessage(dataReceived) {
+    // broadcat the message to all the others clients
+    socket.broadcast.emit("mouseBroadcast", dataReceived);
+  }
+}
